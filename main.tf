e6003ac3902b83170e03b0eafcb8cf57bfe29005
algazali <egz7li@gmail.com>
2021-02-09 12:59:41 +0200

    testing

--- main.tf
+++ main.tf
@@ -2,8 +2,6 @@
 # Create a new instance of the latest RHEL 8 on an
 # t2.micro node with an AWS Tag naming it "odoo"
 provider "aws" {
-  access_key  =  var.my_acess_key
-  secret_key  = var.my_secret_key
   region = "ap-northeast-1"
 }
 
