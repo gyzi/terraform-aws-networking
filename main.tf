da1cef96027cc0cb8fdda63914ed631fe356bc7d
algazali <egz7li@gmail.com>
2021-02-09 12:58:26 +0200

    testing

--- main.tf
+++ main.tf
@@ -7,6 +7,14 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
+data "aws_instance" "i-03ac896b107a2367f" {
+  tags = var.my_instance_tag
+}
+
+
+output "ec2-data" {
+  value = data.aws_instance.i-03ac896b107a2367f
+}
 /* output "ec2-pub-ip" {
     value = aws_instance.web.public_ip
 } */
